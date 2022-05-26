36aa2cf3c23ff4819c7d590ce9cdbbf920e20136
Cesar Salcedo <cesar.salcedo@utec.edu.pe>
2022-05-26 08:51:33 -0500

    Moved random agent to a class and agent's scripts to a new directory

--- agents/random_action.py
+++ agents/random_action.py
@@ -1,17 +1,25 @@
-import gym
 import numpy
+import gym
+
+class RandomAgent():
+    def __init__(self, env):
+        self.action_space = env.action_space
+
+    def act(self, state):
+        return self.action_space.sample()
 
 episodes = 10
 max_iter = 1000
 
 env = gym.make("LunarLander-v2")
+agent = RandomAgent(env)
 
 for episode in range(episodes):
     s = env.reset()
     done = False
 
     for i in range(max_iter):
-        a = env.action_space.sample()
+        a = agent.act(s)
 
         s, r, done, _ = env.step(a)
         env.render()
