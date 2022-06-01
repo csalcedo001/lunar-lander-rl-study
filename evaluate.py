8d6c478691cd8d54e927e36eb9940e15bd872de1
Cesar Salcedo <cesar.salcedo@utec.edu.pe>
2022-06-01 10:32:28 -0500

    Updated evaluate.py to be able to choose agent type and env, and added args validation

--- evaluate.py
+++ evaluate.py
@@ -1,11 +1,22 @@
-import gym
+import os
 from argparse import ArgumentParser
+
+import gym
 
 from agents.reinforce import ReinforceAgent
 from agents.random_action import RandomAgent
 
 
 parser = ArgumentParser()
+parser.add_argument(
+    'agent',
+    type=str)
+parser.add_argument(
+    '--env',
+    type=str, default='LunarLander-v2')
+parser.add_argument(
+    '--checkpoint',
+    type=str, default=None)
 parser.add_argument(
     '--episodes',
     type=int, default=100)
@@ -16,16 +27,65 @@ parser.add_argument(
     '--no-render',
     default=False, action='store_const', const=True)
 
+
+### Get arguments
+
 args = parser.parse_args()
 
+# Main arguments
+env_name = args.env
+agent_name = args.agent
+checkpoint_dir = args.checkpoint
+
+# Optional arguments
 episodes = args.episodes
 max_iter = args.max_iter
 no_render = args.no_render
 
+### Process arguments
 
-env = gym.make('CartPole-v1')
-agent = ReinforceAgent(env)
-agent.load('.')
+# Validate and get agent class
+agent_name_type_map = {
+    'reinforce': ReinforceAgent,
+    'random': RandomAgent,
+}
+
+if agent_name not in agent_name_type_map:
+    raise Exception('Invalid agent: choose from {}.'.format(
+        list(agent_name_type_map.values())))
+
+agent_class = agent_name_type_map[agent_name]
+
+
+# Validate env
+valid_envs = [
+    'CartPole-v1',
+    'LunarLander-v2',
+]
+
+if env_name not in valid_envs:
+    raise Exception('Invalid environment: choose from {}.'.format(
+        valid_envs))
+
+
+# Validate checkpoint for agent type
+load_q = checkpoint_dir != None
+if load_q and agent_name == 'random':
+    raise Exception('Error: random agents cannot load from a checkpoint.')
+
+if load_q and not os.path.isdir(checkpoint_dir):
+    raise Exception('TypeError: checkpoint argument should be a directory.')
+    
+
+
+### Setup for evaluation
+
+env = gym.make(env_name)
+agent = agent_class(env)
+
+if not agent_name == 'random'and checkpoint_dir != None:
+    agent.load(checkpoint_dir)
+
 agent.eval()
 
 for episode in range(episodes):
