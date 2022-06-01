bee4df15c11241b4d43434576be297ba2a976015
Cesar Salcedo <cesar.salcedo@utec.edu.pe>
2022-06-01 11:13:36 -0500

    Made reinforce input adjust to environment, and removed unnecessary code blocks

--- agents/random_action.py
+++ agents/random_action.py
@@ -1,56 +1,11 @@
-import gym
-from argparse import ArgumentParser
-
 from .agent import Agent
 
 
 class RandomAgent(Agent):
     def __init__(self, env):
         super().__init__(env)
-        
+
         self.action_space = env.action_space
 
     def act(self, state):
         return self.action_space.sample()
-
-
-if __name__ == '__main__':
-    parser = ArgumentParser()
-    parser.add_argument(
-        '--episodes',
-        type=int, default=10)
-    parser.add_argument(
-        '--max-iter',
-        type=int, default=1000)
-    parser.add_argument(
-        '--no-render',
-        default=False, action="store_const", const=True)
-
-    args = parser.parse_args()
-
-    episodes = args.episodes
-    max_iter = args.max_iter
-    no_render = args.no_render
-
-
-    env = gym.make('LunarLander-v2')
-    agent = RandomAgent(env)
-
-    for episode in range(episodes):
-        s = env.reset()
-        done = False
-
-        reward = 0
-        for i in range(max_iter):
-            a = agent.act(s)
-
-            s, r, done, _ = env.step(a)
-            reward += r
-
-            if not no_render:
-                env.render()
-
-            if done:
-                break
-
-        print('Episode {}. Reward: {}'.format(episode, reward))
--- agents/reinforce.py
+++ agents/reinforce.py
@@ -1,6 +1,4 @@
-import gym
 import numpy as np
-from argparse import ArgumentParser
 
 import torch
 import torch.nn as nn
@@ -14,8 +12,8 @@ class ReinforceAgent(Agent):
     def __init__(self, env, gamma=0.99, lr=0.01):
         super(ReinforceAgent, self).__init__(env)
 
-        n_in = 4
-        n_out = 2
+        n_in = env.observation_space.shape[0]
+        n_out = env.action_space.n
         
         n_h = 64
 
@@ -92,52 +90,3 @@ class ReinforceAgent(Agent):
         self.optimizer.step()
 
         return loss.item()
-
-
-if __name__ == '__main__':
-    parser = ArgumentParser()
-    parser.add_argument(
-        '--episodes',
-        type=int, default=1000)
-    parser.add_argument(
-        '--max-iter',
-        type=int, default=1000)
-    parser.add_argument(
-        '--no-render',
-        default=False, action="store_const", const=True)
-    
-    args = parser.parse_args()
-    
-    episodes = args.episodes
-    max_iter = args.max_iter
-    no_render = args.no_render
-    
-    
-    env = gym.make('CartPole-v1')
-    agent = ReinforceAgent(env)
-
-    for episode in range(episodes):
-        s = env.reset()
-        done = False
-
-        agent.train_start(s)
-
-        total_reward = 0.
-        for i in range(max_iter):
-            a = agent.act(s)
-    
-            s, r, done, _ = env.step(a)
-            total_reward += r
-
-            agent.train_step(s, r)
-    
-            if not no_render:
-                env.render()
-    
-            if done:
-                break
-        
-        loss = agent.train_end(s)
-        print('Episode {}. Loss: {}. Reward: {}'.format(episode, loss, total_reward))
-    
-    agent.save('.')
